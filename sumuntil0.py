456fddf798f55d0851337dc555bab37e9e132587
Bhumika Khadka <khadkabhumika09@gmail.com>
2026-02-07 13:52:45 -0600

    Add functionality to sum numbers until 0
    Implement a loop to sum numbers until a non-positive number is entered.

---
+++ sumuntil0.py
@@ -0,0 +1,12 @@
+# Get a number input
+n = float(input())
+
+# Initialize the total variable
+total = 0
+
+# Write your code below
+while n>0:
+    total= total+n
+    n=(float(input('Enter a number')))
+
+print(total)
