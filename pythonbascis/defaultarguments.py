21aad3277a9c7d25f6f1730bc97ebd009a16c787
Bhumika Khadka <khadkabhumika09@gmail.com>
2026-02-09 17:35:02 -0600

    Create defaultarguments.py

---
+++ pythonbascis/defaultarguments.py
@@ -0,0 +1,10 @@
+# define the call_me() function with arguments a and b
+def call_me(a='5',b='10'):
+    print(a)
+    print(b)
+
+# take integer input
+n = int(input())
+
+# call call_me() with n as an argument
+call_me(n)
