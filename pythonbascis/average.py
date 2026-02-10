5394cf2f8a462fb09fcb7d6264e69fe3a0d3fb30
Bhumika Khadka <khadkabhumika09@gmail.com>
2026-02-10 16:36:55 -0600

    Create average.py

---
+++ pythonbascis/average.py
@@ -0,0 +1,10 @@
+# take three float input for number1, number2, number3 respectively
+a=float(input("Enter first number"))
+b=float(input("Enter second number"))
+c=float(input("Enter third number"))
+
+# find the average using formula
+average=(a+b+c)/3
+
+# print calculated average
+print(average)
