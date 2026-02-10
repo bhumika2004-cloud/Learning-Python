644bf6645eedea017c76487c14fcde694bb9a720
Bhumika Khadka <khadkabhumika09@gmail.com>
2026-02-09 21:21:48 -0600

    Create profit_loss.py

---
+++ pythonbascis/profit_loss.py
@@ -0,0 +1,7 @@
+# get two input integers for cost_price and selling_price
+cost_price=int(input("Enter the cost price"))
+selling_price=int(input("Enter the selling price"))
+
+# calculate profit or loss amount and print it
+profit_loss= selling_price-cost_price
+print(profit_loss) #negative is loss
