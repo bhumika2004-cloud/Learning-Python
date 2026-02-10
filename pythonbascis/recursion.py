0c9a65ff4c17b2af9185af720185b966ea41ef28
Bhumika Khadka <khadkabhumika09@gmail.com>
2026-02-09 18:00:58 -0600

    Create recursion.py

---
+++ pythonbascis/recursion.py
@@ -0,0 +1,12 @@
+def calculate_factorial(n):
+
+    if n != 1:
+
+        # call calculate_factorial() with appropriate argument
+        return n * calculate_factorial(n-1)
+    
+    return n
+
+number = int(input())
+result = calculate_factorial(number)
+print(result)
