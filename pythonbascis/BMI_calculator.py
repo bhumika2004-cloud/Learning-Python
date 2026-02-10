181cda8eb5d7f2e31e3ae628b22aadeb751028f3
Bhumika Khadka <khadkabhumika09@gmail.com>
2026-02-09 21:24:05 -0600

    Create BMI_calculator.py

---
+++ pythonbascis/BMI_calculator.py
@@ -0,0 +1,11 @@
+# take float input for weight
+weight=float(input("Enter weight of a person in kg"))
+
+# take float input for height
+height=float(input("Enter height of a person in meter"))
+
+# calculate BMI
+BMI= weight/(height**2)
+
+# print the calculated BMI
+print(BMI)
