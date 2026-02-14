860c82137bec3f0b8fcb814d50e951842f54c16a
Bhumika Khadka <khadkabhumika09@gmail.com>
2026-02-14 17:08:48 -0600

    Delete pythonbasics directory

--- pythonbasics/multiplication table.py
+++
@@ -1,8 +0,0 @@
-
-n = int(input())
-
-# use for loop and range() function to loop from 1 to 5
-for i in range (1,6):
-    # multiply user input n with each value of i and print it
-    mt= n*i
-    print(mt)
