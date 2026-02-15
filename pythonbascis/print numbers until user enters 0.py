a6722bfb4480b545a29ea698e1ea37cc5271bb22
Bhumika Khadka <khadkabhumika09@gmail.com>
2026-02-14 18:35:09 -0600

    Create print numbers until user enters 0.py

---
+++ pythonbascis/print numbers until user enters 0.py
@@ -0,0 +1,12 @@
+
+# run a while loop that is always true
+while(True):
+    # take input for number
+    number=int(input("Enter a number"))
+    
+    # terminate the loop if the user input is 0
+    if number==0:
+        break
+
+    # print the number
+    print(number)
