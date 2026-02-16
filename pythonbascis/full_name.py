368cd6be6c88ecc384f7f3809c7518bda05a25bd
Bhumika Khadka <khadkabhumika09@gmail.com>
2026-02-16 17:29:00 -0600

    Create full_name.py

---
+++ pythonbascis/full_name.py
@@ -0,0 +1,11 @@
+# define a function full_name() with two arguments first_name and last_name
+def full_name(first_name,last_name):
+    # print first_name and last_name
+    print(first_name,last_name)
+
+# take user input for first and last name
+first_name=input('Enter first name')
+last_name=input('Enter last name')
+
+# call the function
+full_name(first_name,last_name)
