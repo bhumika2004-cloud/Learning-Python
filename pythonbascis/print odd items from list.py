67ecad2b9d9e7dfa010a58d3306fbc062e284f6a
Bhumika Khadka <khadkabhumika09@gmail.com>
2026-02-17 16:43:47 -0600

    Create print odd items from list.py

---
+++ pythonbascis/print odd items from list.py
@@ -0,0 +1,7 @@
+# create a list of numbers
+numbers=[1,2,3,4,5,6,7,8]
+
+# use for loop to iterate through numbers list
+# inside the loop, only print the odd items on the list
+for i in numbers[0:8:2]:
+    print (i)
