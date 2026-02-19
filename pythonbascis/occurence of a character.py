83722ae74aec09755d7ab1883dfa6d8f95742231
Bhumika Khadka <khadkabhumika09@gmail.com>
2026-02-19 11:46:12 -0600

    Create occurence of a character.py

---
+++ pythonbascis/occurence of a character.py
@@ -0,0 +1,14 @@
+# take string input for string1
+string1= str(input("enter string:"))
+
+# take character input for character1
+character1=(input("enter character:"))
+
+count = 0
+
+# use for loop to iterate through string1
+for i in string1:
+    # check if character is present in the string or not
+    if i==character1:
+        count+=1
+print(count)
