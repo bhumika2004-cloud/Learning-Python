a1df123d79325e88850a90ba2507d966e753b5e4
Bhumika Khadka <khadkabhumika09@gmail.com>
2026-02-19 12:19:19 -0600

    Create string concatenation.py

---
+++ pythonbascis/string concatenation.py
@@ -0,0 +1,8 @@
+# get two string input string1 and string2
+string1=input("Enter string:")
+string2=input("Enter string:")
+# concatenate string1 and string2 and store in the result variable
+result= string1+string2
+
+# print the result variable
+print(result)
