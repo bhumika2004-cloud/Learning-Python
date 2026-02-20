4fc4ddcab15c252ee0e3f40c35453f510d98fcf9
Bhumika Khadka <khadkabhumika09@gmail.com>
2026-02-20 17:53:50 -0600

    Create sqrt.py

---
+++ pythonbascis/sqrt.py
@@ -0,0 +1,9 @@
+# import math module
+import math
+
+# take integer input for number
+number=int(input())
+
+# use sqrt() to find the square root of number and print it
+sq=math.sqrt(number)
+print(sq)
