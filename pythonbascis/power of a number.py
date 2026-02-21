9f4dc211c74eb838200a9e7572f4fe2cfe4660a9
Bhumika Khadka <khadkabhumika09@gmail.com>
2026-02-20 18:45:14 -0600

    Create power of a number.py

---
+++ pythonbascis/power of a number.py
@@ -0,0 +1,10 @@
+# import module math 
+import math
+
+# get an integer user input for num variable
+num = int(input())
+
+# calculate the power of num raised to 4
+# using pow() from module math and print it
+result=math.pow(num,4)
+print(result)
