50a627dad9409efe2cc35990bb065b98088f3713
Bhumika Khadka <khadkabhumika09@gmail.com>
2026-02-20 18:41:46 -0600

    Create standard deviation.py

---
+++ pythonbascis/standard deviation.py
@@ -0,0 +1,10 @@
+
+# import statistics module
+import statistics
+
+# create a data_set variable
+data_set=[2,4,6,8,10]
+
+# use stdev() to find the standard deviation of data_set
+standard_deviation=statistics.stdev(data_set)
+print(standard_deviation)
