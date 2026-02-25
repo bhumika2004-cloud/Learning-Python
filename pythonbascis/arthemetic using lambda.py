3d6cea062386439c82bf37d9ba8de81f74834b67
Bhumika Khadka <khadkabhumika09@gmail.com>
2026-02-25 11:22:22 -0600

    Create arthemetic using lambda.py

---
+++ pythonbascis/arthemetic using lambda.py
@@ -0,0 +1,10 @@
+# create the function
+compute = lambda n1,n2:n1**0.5+n2**0.5
+
+# take two integer inputs
+n1 = int(input())
+n2 = int(input())
+
+# call the function and print the result
+result=compute(n1,n2)
+print(result)
