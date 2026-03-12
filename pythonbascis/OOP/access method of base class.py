cef4dda7a5bcdf019c73a276d683572d1d3ebc17
Bhumika Khadka <khadkabhumika09@gmail.com>
2026-03-11 21:25:43 -0500

    Add Animal and Dog classes with method access

---
+++ pythonbascis/OOP/access method of base class.py
@@ -0,0 +1,15 @@
+# create the Animal class
+class Animal():
+    def eat(self):
+        print('I can eat food')
+
+# create the Dog class
+class Dog(Animal):
+    def bark(self):
+        print('I can bark')
+
+# create an object of Dog
+dog1=Dog()
+
+# call the eat() method using the object
+dog1.eat()
