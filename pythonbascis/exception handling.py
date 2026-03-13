2c112d22a5da6154af55941f7e11cfce2152890a
Bhumika Khadka <khadkabhumika09@gmail.com>
2026-03-13 13:47:40 -0500

    Create exception handling.py

---
+++ pythonbascis/exception handling.py
@@ -0,0 +1,9 @@
+try:
+    numerator = int(input("Enter numerator: "))
+    denominator = int(input("Enter denominator: "))
+
+    result = numerator/denominator
+
+    print(result)
+except:
+    print("Denominator cannot be 0. Try again.")
