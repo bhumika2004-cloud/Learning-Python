0c2a16152d7f0dd9b69ca9ec5fe344dd389b2946
Bhumika Khadka <khadkabhumika09@gmail.com>
2026-03-13 13:49:26 -0500

    Add try-except block for list index handling

---
+++ pythonbascis/try and except.py
@@ -0,0 +1,13 @@
+# create a try block
+try:
+    numbers = [10, 20, 30]
+
+    # take integer input
+    index = int(input())
+
+    # print the item from the number list
+    print(numbers[index])
+
+# create the except block
+except:
+    print('Index is wrong')
