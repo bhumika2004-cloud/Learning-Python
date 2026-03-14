12e5c9fd08a25776f87e053cf1416808be04cca7
Bhumika Khadka <khadkabhumika09@gmail.com>
2026-03-14 12:56:38 -0500

    Create changing directory.py

---
+++ pythonbascis/directories/changing directory.py
@@ -0,0 +1,10 @@
+import os
+
+# print current working directory
+print('Before CWD =',os.getcwd())
+
+# change current working directory
+os.chdir('D:/Projects')
+
+# print current working directory
+print('After CWD =', os.getcwd())
