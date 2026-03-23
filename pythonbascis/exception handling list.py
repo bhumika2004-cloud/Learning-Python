aa47d05be2f735e529e6eb4dbc38535b5b069a47
Bhumika Khadka <khadkabhumika09@gmail.com>
2026-03-22 19:42:20 -0500

    Create exception handling list.py

---
+++ pythonbascis/exception handling list.py
@@ -0,0 +1,12 @@
+# create a try block
+try:
+    cars = ['BMW', 'Ferrari', 'Audi', 'Tesla']
+
+    # take integer input
+    index = int(input())
+
+    # print the item from the cars list
+    print(cars[index])
+# create the except block
+except:
+    print('Wrong Index')
